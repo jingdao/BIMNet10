aa2c3d27b43f4195737b69778406027bb80eb619
jingdao <chenjingdao@wustl.edu>
2018-06-19 17:55:15 -0400

    improve mesh2pcd script

--- mesh2pcd.py
+++ mesh2pcd.py
@@ -30,52 +30,112 @@ def writePCD(filename,subset):
 	f.close()
 	print 'Wrote '+str(len(subset))+' points to '+filename
 
-density=0.01
+def readPLY(filename):
+	vertices = []
+	faces = []
+	numV = 0
+	numF = 0
+	f = open(filename,'r')
+	while True:
+		l = f.readline()
+		if l.startswith('element vertex'):
+			numV = int(l.split()[2])
+		elif l.startswith('element face'):
+			numF = int(l.split()[2])
+		elif l.startswith('end_header'):
+			break
+	for i in range(numV):
+		l = f.readline()
+		vertices.append([float(j) for j in l.split()])
+	for i in range(numF):
+		l = f.readline()
+		faces.append([int(j) for j in l.split()[1:4]])
+	f.close()
+	return vertices,faces
+
+def readOFF(filename):
+	vertices = []
+	faces = []
+	numV = 0
+	numF = 0
+	f = open(filename,'r')
+	l = f.readline()
+	if len(l) > 4: #merged header line
+		l = l[3:]
+	else:
+		l = f.readline()
+	numV = int(l.split()[0])
+	numF = int(l.split()[1])
+	for i in range(numV):
+		l = f.readline()
+		vertices.append([float(j) for j in l.split()])
+	for i in range(numF):
+		l = f.readline()
+		faces.append([int(j) for j in l.split()[1:4]])
+	f.close()
+	return vertices,faces
+
 numVertex=0
 numFace=0
 vertices=[]
 faces=[]
-points=[]
-f=open(sys.argv[1],'r')
-while True:
-	l=f.readline()
-	if l.startswith('element vertex'):
-		numVertex=int(l.split()[2])
-	elif l.startswith('element face'):
-		numFace=int(l.split()[2])
-	elif l.startswith('end_header'):
-		break
 
 for i in range(numVertex):
 	l=f.readline()
 	v=[float(j) for j in l.split()]
 	vertices.append(v)
 
-for i in range(numFace):
-	ll=f.readline().split()
-	p1=numpy.array(vertices[int(ll[1])])
-	p2=numpy.array(vertices[int(ll[2])])
-	p3=numpy.array(vertices[int(ll[3])])
-	v1=p2-p1
-	v2=p3-p1
-	v3=v1+v2
-	area=triangleArea(p1,p2,p3)
-	numSamples = area/density
-	r = numSamples - int(numSamples)
-	numSamples = int(numSamples)
-	if numpy.random.random() < r:
-		numSamples += 1
-	for n in range(numSamples):
-		a=numpy.random.random()
-		b=numpy.random.random()
-		x = p1 + a*v1 + b*v2
-		A1 = triangleArea(p1,p2,x)
-		A2 = triangleArea(p1,p3,x)
-		A3 = triangleArea(p2,p3,x)
-		if abs(A1 + A2 + A3 - area) > 1e-6:
-			x = p1 + v3 - a*v1 - b*v2
-		points.append(x)
+if 'ply' in sys.argv[1]:
+	vertices, faces = readPLY(sys.argv[1])
+elif 'off' in sys.argv[1]:
+	vertices, faces = readOFF(sys.argv[1])
+else:
+	sys.exit(1)
 
-num_resample = 2048
-resample_idx = numpy.random.choice(len(points), num_resample, replace=len(points)<num_resample)
-writePCD(sys.argv[2], numpy.array(points)[resample_idx])
+xmin=min(v[0] for v in vertices)
+xmax=max(v[0] for v in vertices)
+ymin=min(v[1] for v in vertices)
+ymax=max(v[1] for v in vertices)
+zmin=min(v[2] for v in vertices)
+zmax=max(v[2] for v in vertices)
+#density=0.5
+density = 0.01 * max(xmax-xmin,ymax-ymin,zmax-zmin)
+maxPoints = 2048
+
+while True:
+	points=[]
+	for f in faces:
+		p1=numpy.array(vertices[f[0]])
+		p2=numpy.array(vertices[f[1]])
+		p3=numpy.array(vertices[f[2]])
+		v1=p2-p1
+		v2=p3-p1
+		v3=v1+v2
+		area=triangleArea(p1,p2,p3)
+		numSamples = numpy.sqrt(area)/density
+		r = numSamples - int(numSamples)
+		numSamples = int(numSamples)
+		if numpy.random.random() < r:
+			numSamples += 1
+		for n in range(numSamples):
+			a=numpy.random.random()
+			b=numpy.random.random()
+			x = p1 + a*v1 + b*v2
+			A1 = triangleArea(p1,p2,x)
+			A2 = triangleArea(p1,p3,x)
+			A3 = triangleArea(p2,p3,x)
+			if abs(A1 + A2 + A3 - area) > 1e-6:
+				x = p1 + v3 - a*v1 - b*v2
+			points.append(x)
+
+	points = numpy.array(points)
+	if len(points) >= maxPoints:
+		idx = numpy.arange(len(points))
+		numpy.random.shuffle(idx)
+		idx = idx[:maxPoints]
+		points = points[idx,:]
+		writePCD(sys.argv[2],points)
+		break
+	else:
+		density /= 2
+
